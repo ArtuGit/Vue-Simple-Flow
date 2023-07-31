4727b74be519c11d48e068b6190e2d41f7c3ede2
Artu <artuon@gmail.com>
2023-07-31 21:39:05 +0300

    Split menu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,18 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Menu from './Menu.vue'
+</script>
 
 <template>
   <div id="sidebar">
     <h1>Simple Flow</h1>
-    <section class="content-block">
-      <h2>Menu</h2>
-      <nav>
-        <ul>
-          <li><a class="active" href="/">Auth</a></li>
-          <li><a href="/">Add record</a></li>
-          <li><a href="/">All records</a></li>
-        </ul>
-      </nav>
-    </section>
+    <Menu />
   </div>
 </template>
 
@@ -22,12 +15,5 @@
   flex-flow: column nowrap;
   flex: 1 0 10rem;
   align-self: center;
-}
-
-#sidebar h2 {
-  padding-bottom: 1rem;
-  font-size: 1.2rem;
-  font-weight: bold;
-  color: #f8f854;
 }
 </style>
