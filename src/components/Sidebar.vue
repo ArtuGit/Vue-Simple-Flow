dd43a1ae177eb4458d681c25e377b508b7aa2782
Artu <artuon@gmail.com>
2023-07-31 18:00:12 +0300

    Split sidebar and main

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="sidebar">
+    <h1>Simple Flow</h1>
+    <section class="content-block">
+      <h2>Menu</h2>
+      <nav>
+        <ul>
+          <li><a class="active" href="/">Auth</a></li>
+          <li><a href="/">Add record</a></li>
+          <li><a href="/">All records</a></li>
+        </ul>
+      </nav>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
