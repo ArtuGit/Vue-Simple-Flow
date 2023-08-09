64794c8cc5e37a4117dc039d88b85bc536a8677e
Artu <artuon@gmail.com>
2023-08-09 23:03:51 +0300

    Prepare login form

--- src/views/FirstPageView.vue
+++ src/views/FirstPageView.vue
@@ -9,6 +9,25 @@ const { appDescription } = store
   <main class="content-block">
     <h2>Authentication</h2>
     <p>{{ appDescription }}</p>
+
+    <h2>Form</h2>
+    <form action="#" method="post">
+      <div class="form-fields">
+        <div class="form-field">
+          <label class="label" for="text">Username:</label>
+          <input id="text" name="username" type="text" />
+        </div>
+        <div class="form-field">
+          <label class="label" for="password">Password:</label>
+          <input id="password" name="password" type="password" />
+        </div>
+      </div>
+
+      <div class="buttons-margin">
+        <button class="btn" onclick="dummyHandler()">Submit</button>
+        <button class="btn" onclick="dummyHandler()">Cancel</button>
+      </div>
+    </form>
   </main>
 </template>
 
